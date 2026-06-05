47ab84bb22b0a82e17809ee5171e74e5223b54b0
Tessa Debongnie <tessadebongnie@gmail.com>
2026-06-05 14:26:20 +0200

    Update dynamique.go

--- dynamique/dynamique.go
+++ dynamique/dynamique.go
@@ -8,11 +8,12 @@ import (
 
 
 
-func Makelist( donnees map[string]donnees.Coordonnees){
+func Makelist( donnees map[string]donnees.Coordonnees) ([]string){
 	var new []string
 	for pays,_ := range donnees {
 		new = append(new, pays)
 	}
+	return new
 }
 
 func Makeint(ensemble  map[string]string, liste []string)(string){
@@ -43,7 +44,7 @@ func Cout_dynamique( depart string, arrivee string, visitees map[string]string, 
 		for _, pays := range visitees {
 			visitees[pays]="0"
 			nombre1 := Makeint(visitees, liste_pays)
-			calcul := memo_cout[nombre1]+ 
+			calcul := memo_cout[nombre1]
 			glouton.Distance(pays, arrivee, donnees.Pays)
 			
 			if pays != depart && calcul < min_cout {
