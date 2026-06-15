984f512b737db39cec3d24c5803aeff3080d6eb5
Tessa Debongnie <tessadebongnie @gmail.com>
2026-06-15 16:30:20 +0200

    kruskal

--- kruskal/kruskal.go
+++ kruskal/kruskal.go
@@ -88,7 +88,7 @@ func Tri_Arrete(Liste_Arrete []Arrete) []Arrete{
 func Kruskal_privee( donnees map[string]donnees.Coordonnees) ([]Arrete){
 
 	Arrete_triee := Tri_Arrete(Poids_liste(donnees))
-	var Chemin []Arrete
+	var Arbre_liste []Arrete
 	R := make(map[string]string)
 
 
@@ -99,7 +99,7 @@ func Kruskal_privee( donnees map[string]donnees.Coordonnees) ([]Arrete){
 	for i := range Arrete_triee{
 		if R[Arrete_triee[i].Depart] != R[Arrete_triee[i].Arrivee] {
 
-			Chemin = append(Chemin, Arrete_triee[i])
+			Arbre_liste = append(Arbre_liste, Arrete_triee[i])
 			stock := R[Arrete_triee[i].Arrivee]
 			for pays := range donnees {
 				if R[pays] == stock{
@@ -110,70 +110,36 @@ func Kruskal_privee( donnees map[string]donnees.Coordonnees) ([]Arrete){
 		}
 
 	}
-		return Chemin
+		return Arbre_liste
 
 }
 
-func Kruskal(donnees map[string]donnees.Coordonnees )[]string{
 
-	Chemin := Kruskal_privee(donnees)
-	Chemin_pays := make([]string,0)
+func Conversion_dico(Arbre_liste [Arrete])map[string][]string{
+	Arbre_dico := make(map[string][string])
+	for i := range Arbre_liste{
+		Arrete:= Arbre_liste[i]
+		Arbre_dico[Arrete.depart] = append( Arbre_dico[Arrete.depart],Arrete.arrive)
+    	Arbre_dico[Arrete.arrive] = append( Arbre_dico[Arrete.arrive],Arrete.départ)
+	}
 
-	var pays_depart string 
-	var pays_arrivee string
-	compteur := 0
-	
+	return Arbre_dico
+}
 
-	for i := 0; i<len(Chemin);i++{
-		verif1:=false
-		verif2:=false
-
-		for i1:= 0; i1<len(Chemin);i1 ++{
-			if i!=i1 && Chemin[i1].Depart==Chemin[i].Depart {
-				verif1 = true 
-			}
-		}
-		for i2:= 0; i2<len(Chemin);i2 ++{
-			if i!=i2 && Chemin[i2].Arrivee==Chemin[i].Depart {
-				verif1 = true 
-			}
-		}
-
-		for i3:= 0; i3<len(Chemin);i3 ++{
-			if i!=i3 && Chemin[i3].Depart==Chemin[i].Arrivee {
-				verif2 = true 
-			}
-		}
-		for i4:= 0; i4<len(Chemin);i4 ++{
-			if i!=i4 && Chemin[i4].Arrivee==Chemin[i].Arrivee {
-				verif2 = true 
-			}
-		}
-
-		if verif1 && compteur ==0{
-			pays_depart = Chemin[i].Depart
-			compteur =1
-		}else if verif2 && compteur ==0{
-			pays_depart = Chemin[i].Arrivee
-			compteur =1
-		}
-		if verif1 && compteur ==1{
-			pays_arrivee = Chemin[i].Depart
-		}else if verif2 && compteur ==1{
-			pays_arrivee = Chemin[i].Arrivee
+func Parcours(Arbre_dico map[string][]string, Chemin []string, sommet_actuel string, visites map[string]bool) []string{
+	visites[sommet_actuel]=true
+	for i := range Arbre_dico[sommet_actuel]{
+		voisin := Arbre_dico[sommet_actuel][i]
+		if visites[voisin] == false {
+			Chemin = append(Chemin, voisin)
+			Parcours( Arbre_dico, Chemin, voisin)
 		}
 	}
-		Chemin_pays= append(Chemin_pays, pays_depart)
-		for j:= 0; j<len(donnees) ; j++{
-			if Chemin[j].Depart == Chemin_pays[len(Chemin_pays)-1] {
-				Chemin_pays = append(Chemin_pays, Chemin[j].Arrivee)
-				Chemin = slices.Delete(Chemin, j, j+1)
-			}else if Chemin[j].Arrivee == Chemin_pays[len(Chemin_pays)-1]{
-				Chemin_pays = append(Chemin_pays, Chemin[j].Depart)
-				Chemin = slices.Delete(Chemin, j, j+1)			
-			}
-		}
-	Chemin_pays=append(Chemin_pays, pays_arrivee)
+return Chemin
+}
 
-	return Chemin_pays
+func Kruskal_TSP(depart string, donnees map[string]donnees.Coordonnees ) []string{
+	Arbre_liste := Kruskal_privee(donnees)
+	Arbre_dico := Conversion_dico(Arbre_liste)
+	return Parcours( depart, Arbre_dico)
 }
