f92e074eca9eec504f38e31c836fc70ebad8cb47
HAIDA Amina <amina.haida13@gmail.com>
2026-06-25 22:46:09 +0200

    fonction aléatoire

--- cmd/opt_glouton/main.go
+++ cmd/opt_glouton/main.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	"TSP_general/opt_glouton"
 	"TSP_general/glouton"
+	"TSP_general/opt_glouton"
 	"TSP_general/donnees"
 )
 
 func main() {
-
 	trajet := glouton.Fonc_glouton("FR", donnees.Monde)
 
 	trajet_new := opt_glouton.Fonc_optGlouton("FR", donnees.Monde)
 	
 	fmt.Println(trajet)
 	fmt.Println(trajet_new)
-
 }
